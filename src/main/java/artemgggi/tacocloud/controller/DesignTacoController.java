a37be812dddb9c9383d87bf8b53cd04c559c6647
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2023-11-22 13:30:23 +0600

    added jdbc

--- src/main/java/artemgggi/tacocloud/controller/DesignTacoController.java
+++ src/main/java/artemgggi/tacocloud/controller/DesignTacoController.java
@@ -3,6 +3,7 @@ package artemgggi.tacocloud.controller;
 import artemgggi.tacocloud.model.Ingredient;
 import artemgggi.tacocloud.model.Taco;
 import artemgggi.tacocloud.model.TacoOrder;
+import artemgggi.tacocloud.repository.IngredientRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import artemgggi.tacocloud.model.Ingredient.Type;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,24 +21,19 @@ import java.util.stream.Collectors;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepo;
+
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
         Type[] types = Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
+                    filterByType((List<Ingredient>) ingredients, type));
         }
     }
     @ModelAttribute(name = "tacoOrder")
--- src/main/java/artemgggi/tacocloud/repository/JdbcIngredientRepository.java
+++ src/main/java/artemgggi/tacocloud/repository/JdbcIngredientRepository.java
@@ -4,6 +4,9 @@ import artemgggi.tacocloud.model.Ingredient;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,16 +20,37 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        return Optional.empty();
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
+        return results.size() == 0 ?
+                Optional.empty() :
+                Optional.of(results.get(0));
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbcTemplate.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum)
+            throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type")));
     }
 }
