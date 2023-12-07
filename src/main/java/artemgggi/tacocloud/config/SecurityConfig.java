3fecf6fc18c796886a0b464db190657062e44081
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2023-12-07 18:44:31 +0600

    added registration controller

--- src/main/java/artemgggi/tacocloud/config/SecurityConfig.java
+++ src/main/java/artemgggi/tacocloud/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import artemgggi.tacocloud.model.User;
 import artemgggi.tacocloud.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +25,15 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**").permitAll()
+                .and()
+                .build();
     }
 }
