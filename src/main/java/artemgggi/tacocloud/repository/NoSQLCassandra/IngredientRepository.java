e9b94caa7a88a927c79bc19548f46c002264ee19
artemgggi <artem.ivanchikov.99@mail.ru>
2023-12-25 12:02:43 +0600

    deleted NoSQL folders

--- src/main/java/artemgggi/tacocloud/repository/NoSQLCassandra/IngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package artemgggi.tacocloud.repository.NoSQLCassandra;
-
-import artemgggi.tacocloud.model.NoSQLCassandra.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepository
-        extends CrudRepository<Ingredient, String> {
-}
