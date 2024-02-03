50e60d9d64b8453d5251a128c7d67a200fce8274
artemgggi <artem.ivanchikov.99@mail.ru>
2024-02-03 19:34:52 +0200

    added actuator

--- src/main/java/artemgggi/tacocloud/service/OrderReceiverService.java
+++ src/main/java/artemgggi/tacocloud/service/OrderReceiverService.java
@@ -1,9 +1,8 @@
 package artemgggi.tacocloud.service;
 
 import artemgggi.tacocloud.model.TacoOrder;
-import jakarta.jms.JMSException;
 
 public interface OrderReceiverService {
 
-    TacoOrder receiveOrder() throws JMSException;
+    TacoOrder receiveOrder();
 }
