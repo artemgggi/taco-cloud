a2d22b0d02c5e17a9efb4006108920541c2a1ee8
artemgggi <artem.ivanchikov.99@mail.ru>
2024-02-04 19:55:54 +0200

    refactored actuator properties

--- src/main/java/artemgggi/tacocloud/config/KafkaConfig.java
+++ src/main/java/artemgggi/tacocloud/config/KafkaConfig.java
@@ -1,18 +1,18 @@
-//package artemgggi.tacocloud.config;
-//
-//import org.apache.kafka.clients.admin.NewTopic;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.config.TopicBuilder;
-//
-//@Configuration
-//public class KafkaConfig {
-//
-//    @Bean
-//    public NewTopic createNgDevTopic() {
-//        return TopicBuilder.name("TestTopic")
-//                .partitions(3)
-//                .replicas(3)
-//                .build();
-//    }
-//}
+package artemgggi.tacocloud.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic createNgDevTopic() {
+        return TopicBuilder.name("TestTopic")
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+}
